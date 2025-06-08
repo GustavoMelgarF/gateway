6ac05ddb150723f456a96381e79640b79987da09
Gustavo Melgar <gustavomelgarf@gmail.com>
2025-06-08 20:11:40 +0200

    Initial commit

---
+++ src/test/java/com/relatos_de_papel/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relatos_de_papel.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
